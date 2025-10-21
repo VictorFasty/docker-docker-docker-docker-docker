ea34e7e5c332e28933fe540494b18c5c1bf4784b
victornext <victorbrs517@gmail.com>
2025-10-21 19:43:23 -0300

    dockerfile

---
+++ convidados/Dockerfile
@@ -0,0 +1,7 @@
+FROM openjdk:17
+
+WORKDIR /app
+
+COPY ./target/convidados-0.0.1-SNAPSHOT.jar .
+
+ENTRYPOINT java -jar convidados-0.0.1-SNAPSHOT.jar
--- convidados/src/main/java/io/github/VictorFasty/convidados/convidado.java
+++ convidados/src/main/java/io/github/VictorFasty/convidados/convidado.java
@@ -2,10 +2,12 @@ package io.github.VictorFasty.convidados;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class convidado {
     private String nome;
-    private String cpf;    
+    private String cpf;
 }
